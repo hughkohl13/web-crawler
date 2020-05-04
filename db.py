2061137656a2977abc562553dcc206fac6184de8
Hugh Kohl <hgkohl@us.ibm.com>
2020-05-04 13:08:58 -0700

    cleaning up files

--- db.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-cluster = pymongo.MongoClient('mongodb+srv://hughkohl:*Tiger13@cluster0-bdg3q.azure.mongodb.net/test?retryWrites=true&w=majority')
-db = cluster["job_scrape"]
-collection = db["scrape"]
-
-post = {"role": "bacon", "company": "lettuce", "url": "tomato"}
-
-collection.insert_one(post)
